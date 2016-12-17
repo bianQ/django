69d82c3dd481a24eca384033368e6c6981e3484b
bianQ <vagaab@foxmail.com>
2016-12-17 21:14:28 +0800

    添加依赖包安装

--- DjangoSetup/setup_python.sh
+++ DjangoSetup/setup_python.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum install -y gcc python-devel zlib-devel openssl-devel
+yum install -y gcc gcc-c++ python-devel zlib-devel openssl-devel vim
 wget https://www.python.org/ftp/python/3.5.2/Python-3.5.2.tgz
 wget https://pypi.python.org/packages/dc/37/f01d823fd312ba8ea6c3aea906d2d6ac9e9e8bf9e7853e34f296e94b0d0d/setuptools-32.0.0.tar.gz#md5=e5f513a5b53e843b361d663feec4f5fa --no-check-certificate
 wget https://pypi.python.org/packages/11/b6/abcb525026a4be042b486df43905d6893fb04f05aac21c32c638e939e447/pip-9.0.1.tar.gz#md5=35f01da33009719497f01a4ba69d63c9 --no-check-certificate
